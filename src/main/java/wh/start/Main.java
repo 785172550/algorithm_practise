f0c3b40d1e78d771d9eee315df7f132d22203548
kenneth <785172550@qq.com>
2019-06-02 11:46:57 +0800

    init algorithm practise repos

---
+++ src/main/java/wh/start/Main.java
@@ -0,0 +1,10 @@
+package wh.start;
+
+import com.google.common.collect.Lists;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(Lists.newArrayList(1,2,3,4,5));
+    }
+}
---
+++ src/main/python/wh/start/main.py
@@ -0,0 +1,10 @@
+# coding:utf-8
+# life is short, you need Python！
+
+
+def start():
+    print('start...')
+
+
+if __name__ == "__main__":
+    start()
