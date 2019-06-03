e9169c4f1a76165a75a8435804ea34ff31622744
Kenneth.Wang <785172550@qq.com>
2019-06-03 10:02:43 +0800

    Create TypeUtils.java
    add TypeUtils.java

---
+++ src/main/java/utils/TypeUtils.java
@@ -0,0 +1,44 @@
+package util;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.IntFunction;
+import java.util.stream.Collectors;
+
+public class TypeUtil {
+
+	private TypeUtil() {
+	}
+
+	/**
+	 * convert List<T> to List<U>
+	 * 
+	 * @param from
+	 *            list<T> to be
+	 * @param func
+	 *            a method to convert T to U
+	 * @return List<U>
+	 */
+	public static <T, U> List<U> convertList(List<T> from, Function<T, U> func) {
+		// return from.stream().map(func).collect(Collectors.toList());
+		return from.stream().map(func).collect(Collectors.toList());
+	}
+
+	/**
+	 * 
+	 * @param from
+	 *            array of T elements
+	 * @param func
+	 *            a method to convert T to U
+	 * @param generator
+	 *            a function which produces a new array of the desired type and the
+	 *            provided length
+	 * @return U[]
+	 */
+	public static <T, U> U[] convertArray(T[] from, Function<T, U> func, IntFunction<U[]> generator) {
+		// return Arrays.stream(from).map(func).toArray(generator);
+		return Arrays.stream(from).map(func).toArray(generator);
+	}
+
+}
