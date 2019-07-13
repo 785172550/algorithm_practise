d9b0266f32aebf1c27cc73897dbc45bb305bf71f
Kenneth.Wang <785172550@qq.com>
2019-07-13 17:06:54 +0800

    Create SymmeticTree.java

---
+++ src/main/java/leetcode/SymmeticTree.java
@@ -0,0 +1,33 @@
+package com.wh.test.leetcode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+import com.wh.test.tree.TreeNode;
+
+/**
+ * Definition for a binary tree node. public class TreeNode { int val; TreeNode left; TreeNode
+ * right; TreeNode(int x) { val = x; } }
+ */
+public class SymmeticTree {
+  public boolean isSymmetric(TreeNode root) {
+    Queue<TreeNode> queue = new LinkedList<TreeNode>();
+    if (root == null) return true;
+    queue.add(root.left);
+    queue.add(root.right);
+    while (queue.size() > 1) {
+      TreeNode left = queue.poll();
+      TreeNode right = queue.poll();
+      if (left == null && right == null) continue;
+      if (left == null ^ right == null) return false;  // key step
+      if (left.val != right.val) return false;
+
+      queue.add(left.left);
+      queue.add(right.right);
+
+      queue.add(left.right);
+      queue.add(right.left);
+    }
+    return true;
+  }
+}
